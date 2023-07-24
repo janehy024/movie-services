48fbc1488757fa7411558af36c261873a6e38610
장서윤 <seoy316@naver.com>
2023-07-24 10:15:26 +0900

    feat: 새로 생성한 패키지 component scan 범위 포함

--- src/main/java/movie/movieservice/MovieServiceApplication.java
+++ src/main/java/movie/movieservice/MovieServiceApplication.java
@@ -1,20 +1,16 @@
 package movie.movieservice;
 
-import api.MovieData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"movie.api", "movie.movieservice"})
 public class MovieServiceApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(MovieServiceApplication.class, args);
-
-		MovieData movieData = new MovieData();
-
-		movieData.callApiWithJson();
-
 	}
 
 }
