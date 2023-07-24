64523ad0fc6fc63469e63224b7dc8d19f93c7ea2
장서윤 <seoy316@naver.com>
2023-07-24 10:08:55 +0900

    fix: EntityManager 생성자 주입으로 변경

--- src/main/java/movie/movieservice/repository/MovieRepository.java
+++ src/main/java/movie/movieservice/repository/MovieRepository.java
@@ -1,24 +1,26 @@
 package movie.movieservice.repository;
 
-import lombok.RequiredArgsConstructor;
-import movie.movieservice.domain.Actor;
 import movie.movieservice.domain.Movie;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 @Repository
-@RequiredArgsConstructor
 public class MovieRepository {
 
     @PersistenceContext
     private final EntityManager em;
+
+    public MovieRepository(EntityManager em) {
+        this.em = em;
+    }
 
     public String save(Movie movie){
         em.persist(movie);
         return movie.getId();
     }
 
-    public Actor findOne(Long id){ return em.find(Actor.class, id); }
+    public Movie findOne(Long id){ return em.find(Movie.class, id); }
 }
